def7a496412060f06dfe2fe9e260ab50358b7668
Vijay Kumar <118923157+vijayvrx8055@users.noreply.github.com>
2025-07-27 19:06:09 +0530

    Initial commit

---
+++ skill-sage-service-app/src/main/java/com/vrx/skillsage/SkillSageServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.vrx.skillsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSageServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSageServiceAppApplication.class, args);
+	}
+
+}
---
+++ skill-sage-service-app/src/test/java/com/vrx/skillsage/SkillSageServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrx.skillsage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSageServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
